f957eadaace56db92f54d4f70c04401c3647d1be
jeapark <jpark623@uw.edu>
2023-02-09 09:14:25 -0800

    Update get_fig5a_b.r

--- R/get_fig5a_b.r
+++ R/get_fig5a_b.r
@@ -1,6 +1,7 @@
 #' Sci Data Paper Figure 5a/5b
 #'
-#' @import gridtext
+#' @import pheatmap
+#' @import RColorBrewer
 #'
 #' @return figure5a_b.pdf
 #' @export
@@ -33,34 +34,56 @@ get_fig5a_b=function() {
     geom_point() + theme_minimal() + ggrepel::geom_text_repel(size=3) +
     scale_color_manual(values=c("#005AB5", "black", "#DC3220")) +
     geom_vline(xintercept=c(-1, 1), col="green") +
-    geom_hline(yintercept=-log10(0.05), col="green") + xlab(NULL) +
+    geom_hline(yintercept=-log10(0.05), col="green") + xlab("log2 Fold Change") +
     ylab("-log(adjusted p-value)") + theme(legend.position="none",
                                            axis.text=element_text(size=rel(0.55)))
   #axis.text=element_text(size=10))
   
-  # wrangle data for lollipop plot
-  temp <- diffTab[diffTab$diffexpressed != "NO",]
-  temp <- temp[order(temp$AutoDom.AD),]
-  temp$Protein <- factor(temp$Protein, levels=temp$Protein)
+  data("metaDT", envir=environment())
+  metaDT <- metaDT[1:62,]
+  rownames(metaDT) <- metaDT$Replicate
+  idx <- grep("High|Sporadic", metaDT$Condition)
+  metaDT <- metaDT[-idx,]
+  data("noBatchProt", envir=environment())
+  noBatchProt <- noBatchProt[,-idx]
+  data("diffTab", envir=environment())
   
-  # lollipop plot
-  p4 <- ggplot(temp, aes(x=Protein, y=AutoDom.AD,
-                         label=round(AutoDom.AD, digits=2))) +
-    geom_point(stat='identity', aes(col=diffexpressed), 
-               #size=6) +
-               size=2) +
-    scale_color_manual(name="AutoDom ADD",
-                       labels=c("Under Abundant", "Over Abundant"),
-                       values=c("UP"="#DC3220", "DOWN"="#005AB5")) +
-    geom_text(color="white", size=2) +
-    labs(title= NULL, y=NULL, x="Protein Group",
-         subtitle=NULL) +
-    coord_flip() + theme_bw()+ theme(axis.text=element_text(size=rel(0.55)))
-  #theme(axis.text.x = element_text(size = 10))
+  # rename condition groups
+  metaDT$Condition = gsub("Path", "ADNC", gsub("Control ", "HCF - ", 
+                                               gsub("AD", "ADD", metaDT$Condition)))
   
-  ytext <- gridtext::richtext_grob("log2 Fold Change")
+  # filter data with fdr < 0.05 and logFC > 1
+  temp <- diffTab[diffTab$adj.P.Val < 0.05,]
+  temp <- temp[abs(temp$AutoDom.AD) > 1, ]
+  sigNames <- rownames(temp)
+  protSig <- noBatchProt[rownames(noBatchProt) %in% sigNames,
+                         grep("TZR",colnames(noBatchProt))]
+  rownames(protSig) <- sapply(strsplit(sapply(strsplit(rownames(protSig)," @ "),
+                                              "[", 2), ","), "[", 1)
+  rownames(protSig) <- sapply(strsplit(rownames(protSig), "|",fixed=T), "[", 3)
   
-  p5 <- grid.arrange(p0, p4, ncol=2,   bottom=ytext)
+  # heatmap factors to be shown
+  annotation <- data.frame(Condition=factor(metaDT$Condition),
+                           `Cognitive Status`=factor(metaDT$`Cognitive Status`),
+                           Age=metaDT$Age,
+                           Sex=factor(metaDT$Sex),
+                           Batch=factor(metaDT$Batch))
+  rownames(annotation)=metaDT$Replicate
+  heat_colors <- brewer.pal(11, "RdBu")
+  annot_colors=list(Condition=c(`AutoDom ADD`="#ff4500",
+                                `HCF - Low ADNC`="#1874cd"))
+  
+  p4 <- ggplotify::as.ggplot(pheatmap(protSig, color=heat_colors,
+                             cluster_rows=T, show_rownames=T,
+                             annotation_col=annotation,
+                             annotation_colors=annot_colors,
+                             #clustering_distance_rows="correlation",
+                             border_color=NA, fontsize=7, scale="row",
+                             fontsize_row=5, height=20, show_colnames=F))
+  
+  p5 = grid.arrange(p0,p4,ncol=2)
+  
+
   
   ggsave("fig/figure5a_b.pdf",p5, dpi=600, dev='pdf',
          height=9, width=15, units="in")
